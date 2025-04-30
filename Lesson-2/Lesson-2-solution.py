7465cecda65630e56ac80ab794e66c1edbc3505f
Logan Dihel <logan.dihel@gmail.com>
2025-04-30 18:25:27 -0400

    forgot to plot third graph

--- Lesson-2/Lesson-2-solution.py
+++ Lesson-2/Lesson-2-solution.py
@@ -125,5 +125,7 @@ if __name__ == "__main__":
     plt.title("Input vs Time")
     plt.xlabel("Time (s)")
     plt.ylabel("Input (m/s^2)")
-    
+    plt.grid()
+    plt.legend()
+    plt.show()
     
