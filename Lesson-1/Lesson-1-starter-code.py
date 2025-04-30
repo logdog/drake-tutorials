86b922338b8b00ecb9bc3d218ed7ec6e76317d0c
Logan Dihel <logan.dihel@gmail.com>
2025-04-30 16:10:10 -0400

    Lesson 1 complete

---
+++ Lesson-1/Lesson-1-starter-code.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pydrake.all import (
+    plot_system_graphviz,
+    ConstantVectorSource,
+    DiagramBuilder,
+    Simulator,
+    LeafSystem,
+    LogVectorOutput,
+)
+
+# double integrator system with a continuous state
+class DoubleIntegrator(LeafSystem):
+    
+    def __init__(self):   
+        LeafSystem.__init__(self)
+        
+        # declare the input port (1-dimensional)
+        self.DeclareVectorInputPort("u", 1)
+        
+        # declare the continuous state (1 position state, 1 velocity state, 0 abstract states)
+        self.state_index = self.DeclareContinuousState(1,1,0)
+        
+        # declare the output port to be the state vector
+        self.DeclareStateOutputPort("y", self.state_index)
+        
+    def DoCalcTimeDerivatives(self, context, derivatives):
+        # read the input
+        u = self.get_input_port().Eval(context)[0]
+        
+        # read the output
+        state = context.get_continuous_state().get_vector()
+        x = state.GetAtIndex(0)
+        x_dot = state.GetAtIndex(1)
+        
+        # state space equations
+        x_ddot = u
+        
+        # update the derivatives for the continous-time integrator
+        # print(f"DoCalcTimeDerivatives(): t={context.get_time()}, u={u}, x={x}, x_dot={x_dot}, x_ddot={x_ddot}")
+        derivatives.get_mutable_vector().SetFromVector(
+            np.array([x_dot, x_ddot])
+        )
+        
+if __name__ == "__main__":
+    
+    # create the diagram
+    builder = DiagramBuilder()
+    plant = builder.AddSystem(DoubleIntegrator())
+    plant.set_name("double integrator")
+    
+    vector_input = builder.AddSystem(ConstantVectorSource(np.array([-1.0])))
+    vector_input.set_name("constant vector input source")
+    
+    builder.Connect(vector_input.get_output_port(), plant.get_input_port())
+    
+    logger = LogVectorOutput(plant.get_output_port(), builder)
+    logger.set_name("output state logger")
+    
+    diagram = builder.Build()
+    diagram.set_name("Double Integrator System")
+    
+    # visualize the diagram (optional)
+    plot_system_graphviz(diagram)
+    plt.show()
+    
+    # set initial conditions
+    context = diagram.CreateDefaultContext()
+    context.SetTime(0.0)
+    context.SetContinuousState(np.array([1.0, 2.0]))
+    
+    # create the simulator
+    simulator = Simulator(diagram, context)
+    simulator.set_target_realtime_rate(1.0)
+    
+    # run the simulation
+    print("Running simulation...")
+    simulator.AdvanceTo(5.0)
+    print("Simulation complete. Press Ctrl+C to exit.")
+    
+    # create plots
+    log = logger.FindLog(context)
+    
+    x = log.data()[0,:]
+    x_dot = log.data()[1,:]
+    
+    plt.figure()
+    plt.plot(log.sample_times(), x, label="x")
+    plt.title("Position vs Time")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Position (m)")
+    plt.grid()
+    plt.legend()
+    plt.show()
+    
+    plt.figure()
+    plt.plot(log.sample_times(), x_dot, label="x_dot")
+    plt.title("Velocity vs Time")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Velocity (m/s)")
+    plt.grid()
+    plt.legend()
+    plt.show()
--- Lesson-1/README.md
+++ Lesson-1/README.md
@@ -1,2 +1,44 @@
 ## Lesson 1: Simulate Continuous Time ODE
 
+In this lesson, you will learn to model systems of the form
+
+$$
+\dot x = f(x, u) \\
+$$x(0) = x_0$$ 
+$$
+
+where $$x_0$$ is given as some initial condition and $$u$$ is the input. In Drake, you would define this as a [LeafSystem](https://drake.mit.edu/doxygen_cxx/classdrake_1_1systems_1_1_leaf_system.html). 
+
+> Note: I will provide links to the documentaiton but it will take time to understand how to read the documentation, especially because we are using python but the documentation is auto-generated from C++ code. Almost everything in the C++ documentation has been ported to python using bindings, but not everything. If you need help seeing how something is done in Python, search the [Drake github page](https://github.com/RobotLocomotion/drake). For your first pass through this tutorial, you may skip the documentation, but later on you should at least become familiar with all the functions you use in Drake so you know what else is available.
+
+Specifically, you will simulate a double integrator with dynamics:
+
+$$
+\dot{x}_1 = x_2 \\
+\dot{x}_2 = u
+$$
+
+with $$u(t) = -1$$ for sake of example. Your task is to go through the code and understand it. When you are done, you will be asked to make the following changes:
+
+## Tasks
+
+1. Change the initial conditions and constant input vector to something else.
+
+2. Modify the system dynamics to change the block's mass and/or incorporate linear viscuous drag force (proportional to the velocity)
+
+3. Replace the `ConstantVectorSource` with your own leaf system that outputs a sinusoid. Connect the output of this leaf system to the double integrator. Also, add another logger to visualize the input to the double integrator. *Hint: here is some more starter code.*
+
+```python
+
+class MyReferenceSignal(LeafSystem):
+    
+    def __init__(self):   
+        LeafSystem.__init__(self)
+        self.DeclareVectorOutputPort("u", 1, self.MyOutput)
+
+    def MyOutput(self, context, output):
+        t = context.get_time()
+        u = 10*t # modify this line
+        output.SetFromVector([u])
+```
+
