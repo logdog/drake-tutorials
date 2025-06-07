0c444d671d7b71d60810e52763ebb97c17a8042d
Logan Dihel <ldihel@purdue.edu>
2025-06-06 21:58:12 -0400

    added plotting for control signal

--- Lesson-1/Lesson-1-starter-code.py
+++ Lesson-1/Lesson-1-starter-code.py
@@ -35,13 +35,25 @@ class DoubleIntegrator(LeafSystem):
         x_dot = state.GetAtIndex(1)
         
         # state space equations
-        x_ddot = u
+        x_ddot = 1/2 * (-0.5 * x_dot + u)
         
         # update the derivatives for the continous-time integrator
         # print(f"DoCalcTimeDerivatives(): t={context.get_time()}, u={u}, x={x}, x_dot={x_dot}, x_ddot={x_ddot}")
         derivatives.get_mutable_vector().SetFromVector(
             np.array([x_dot, x_ddot])
         )
+
+
+class Controller(LeafSystem):
+    
+    def __init__(self):   
+        LeafSystem.__init__(self)
+        self.DeclareVectorOutputPort("u", 1, self.MyOutput)
+
+    def MyOutput(self, context, output):
+        t = context.get_time()
+        u = np.sin(t) # modify this line
+        output.SetFromVector([u])
         
 if __name__ == "__main__":
     
@@ -50,32 +62,34 @@ if __name__ == "__main__":
     plant = builder.AddSystem(DoubleIntegrator())
     plant.set_name("double integrator")
     
-    vector_input = builder.AddSystem(ConstantVectorSource(np.array([-1.0])))
-    vector_input.set_name("constant vector input source")
+    controller = builder.AddSystem(Controller())
     
-    builder.Connect(vector_input.get_output_port(), plant.get_input_port())
+    builder.Connect(controller.get_output_port(), plant.get_input_port())
     
     logger = LogVectorOutput(plant.get_output_port(), builder)
+    # logger = LogVectorOutput(plant.get_output_port(), builder, publish_period=0.1) # if you want to log only at specific intervals
     logger.set_name("output state logger")
+
+    input_logger = LogVectorOutput(controller.get_output_port(), builder)
     
     diagram = builder.Build()
     diagram.set_name("Double Integrator System")
     
     # visualize the diagram (optional)
-    plot_system_graphviz(diagram)
-    plt.show()
+    # plot_system_graphviz(diagram)
+    # plt.show()
     
     # set initial conditions
     context = diagram.CreateDefaultContext()
     context.SetTime(0.0)
-    context.SetContinuousState(np.array([1.0, 2.0]))
+    context.SetContinuousState(np.array([0.0, 0.0]))
     
     # create the simulator
     simulator = Simulator(diagram, context)
     
     # run the simulation
     print("Running simulation...")
-    simulator.AdvanceTo(5.0)
+    simulator.AdvanceTo(4*np.pi)
     print("Simulation complete. Press Ctrl+C to exit.")
     
     # create plots
@@ -98,6 +112,17 @@ if __name__ == "__main__":
     plt.title("Velocity vs Time")
     plt.xlabel("Time (s)")
     plt.ylabel("Velocity (m/s)")
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+    input_log = input_logger.FindLog(context)
+    u = input_log.data()[0,:]
+    plt.figure()
+    plt.plot(input_log.sample_times(), u, label="u")
+    plt.title("Control Input vs Time")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Control Input (N)")
     plt.grid()
     plt.legend()
     plt.show()
